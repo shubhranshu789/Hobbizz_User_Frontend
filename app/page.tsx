316a39e74d335c5fa1ae14d5ee0e2920dfd7f7f2
shubhranshu789 <66314743+shubhranshu789@users.noreply.github.com>
2025-06-04 12:54:47 +0530

    'Update'

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
-import Image from "next/image";
+'use client';
 
-export default function Home() {
+import { useRouter } from 'next/navigation';
+
+export default function Page() {
+  const router = useRouter();
+    const data = 20;
+  const str = "shubh"
+
+  const handleClick = () => {
+   router.push(`./Components/home?data=${data}&str=${str}`);
+  };
+  const handleClick2 = () => {
+    router.push('./Components/About');
+  };
+
+
   return (
     <div>
-      <h1>Hello Shubh</h1>
+      <h1>Welcome to the Main Page</h1>
+      <button onClick={handleClick}>Go to Home</button>
+      {/* <button onClick={handleClick2}>Go to About</button> */}
     </div>
   );
 }
