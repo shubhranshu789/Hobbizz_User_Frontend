dff4dedf0eeb206a860566087eb8106c4bc6dd89
suhelsaifi0 <suhelsaifi0001@gmail.com>
2025-06-23 21:24:57 +0530

    Added Code

--- app/Components/ArtClub/ClubNews/page.tsx
+++ app/Components/ArtClub/ClubNews/page.tsx
@@ -390,7 +390,7 @@ const ClubNewsForm = () => {
       </div>
 
       <div>
-        {/* Footer */}
+        {/* Footer  -> This is a Fotter*/ } 
         <footer className="bg-slate-800 text-white">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
             <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
